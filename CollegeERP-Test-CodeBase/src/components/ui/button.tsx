60c0c70541aa709d845787826f9d4c93bd0ec703
Lovesh Purswani <loveshpradeeppurswani@gmail.com>
2024-07-11 21:43:34 +0530

    Added Button Component

--- CollegeERP-Test-CodeBase/src/components/ui/button.tsx
+++ CollegeERP-Test-CodeBase/src/components/ui/button.tsx
@@ -3,14 +3,16 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+// import { cn } from "@/lib/utils"
 // import { cn } from "../../lib/utils"
 
+// This is for declaring variables and creating tailwind classes
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-[green] text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -19,6 +21,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        primary:"bg-black text-white"
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -45,7 +48,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant:"default", size, className:"text-red-500"}))} // This is where - add tailwind classes and varibales
         ref={ref}
         {...props}
       />
