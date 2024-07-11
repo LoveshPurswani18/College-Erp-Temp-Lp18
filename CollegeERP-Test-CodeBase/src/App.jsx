7dbabdac29d7741dc53184e5dcee66cde33da356
Lovesh Purswani <loveshpradeeppurswani@gmail.com>
2024-07-11 17:47:02 +0530

    Added NavBar component

--- CollegeERP-Test-CodeBase/src/App.jsx
+++ CollegeERP-Test-CodeBase/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import NavBar from './components/NavBar'
 import './App.css'
+
 
 function App() {
 
   return (
     <>
     <h1>College ERP</h1>
+    <header>
+      <NavBar/>
+    </header>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ CollegeERP-Test-CodeBase/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar(){
+
+    return(
+        // <h5>NavBar</h5>
+        <span className="Nav">
+            Nav Bar
+        </span>
+    );
+};
+
+export default NavBar;
